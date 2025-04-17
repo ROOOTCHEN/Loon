bfa1a0b59ddf53679e20dcaaa20cfa6309ae5c92
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2025-04-17 15:16:56 +0800

    Update DurexTools.js

--- Durex/DurexTools.js
+++ Durex/DurexTools.js
@@ -1,20 +1,31 @@
-if ($request && $request.headers) {
-    const accessToken = $request.headers['Access-Token'];
-    if (accessToken) {
-        $notification.post("获取 Access-Token", `Access-Token: ${accessToken}`, "点击复制到剪贴板", {
-            "clipboard": accessToken
-        });
-    }
-}
+if ($request && $request.url) {
+    const url = $request.url;
 
-if ($request && $request.method !== 'OPTIONS') {
-    let url = $request.url;
-    let snIdMatch = url.match(/[?&]snId=(\d+)/);
-    if (snIdMatch) {
-        let snId = snIdMatch[1];
-        $notification.post("检测到活动 SNID", `SNID: ${snId}`, "点击复制到剪贴板", {
-            "clipboard": snId
-        });
+    // 处理 Access-Token 只针对 /mp/user/info
+    if (url.includes("/mp/user/info")) {
+        const accessToken = $request.headers['Access-Token'];
+        if (accessToken) {
+            $notification.post(
+                "获取 Access-Token",
+                `Access-Token: ${accessToken}`,
+                "点击复制到剪贴板",
+                { "clipboard": accessToken }
+            );
+        }
+    }
+
+    // 处理 SNID 只针对 /mp/activity.lottery/getUserInfoV2
+    else if (url.includes("/mp/activity.lottery.getUserInfoV2")) {
+        let snIdMatch = url.match(/[?&]snId=(\d+)/);
+        if (snIdMatch) {
+            let snId = snIdMatch[1];
+            $notification.post(
+                "检测到活动 SNID",
+                `SNID: ${snId}`,
+                "点击复制到剪贴板",
+                { "clipboard": snId }
+            );
+        }
     }
 }
 
