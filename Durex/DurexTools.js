d7ed01ad5c4beb28e589f4d560adfcec99aeaa2d
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2025-12-02 14:39:37 +0800

    Update DurexTools.js

--- Durex/DurexTools.js
+++ Durex/DurexTools.js
@@ -1,8 +1,3 @@
-/**
- * @fileoverview Durex 一体脚本：提取 Access-Token、SNID，同时支持重定向 GoodsID（根据 Durex_GoodsID 输入）
- * @author ROOOT
- */
-
 if ($request && $request.url) {
 	const url = $request.url;
 	const headers = $request.headers;
@@ -21,19 +16,7 @@ if ($request && $request.url) {
 		}
 	}
 
-	// ✅ SNID 提取（仅限 /mp/activity.lottery/getUserInfoV2?snId=xxx）
-	else if (/\/mp\/activity\.lottery\/content/i.test(url)) {
-		const snIdMatch = url.match(/[?&]snId=([^&]+)/);
-		if (snIdMatch) {
-			const snId = snIdMatch[1];
-			$notification.post(
-				"检测到活动 SNID",
-				`SNID: ${snId}`,
-				"点击复制到剪贴板", {
-					clipboard: snId
-				}
-			);
-		}
+
 	}
 
 	// ✅ GoodsID 重定向（仅限 /mp/points.goods/detailMulSpec?goodsId=*）
